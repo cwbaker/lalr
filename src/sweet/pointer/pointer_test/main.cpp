383a0422080363f046146363f0d1226f81a4ca08
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-01-09 20:45:04 +1300

    Remove unused pointer library (use shared_ptr<> instead)

--- src/sweet/pointer/pointer_test/main.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <unit/UnitTest.h>
-#include <unit/TestReporterStdout.h>
-
-int main( int /*argc*/, char** /*argv*/ )
-{
-    return UnitTest::RunAllTests();
-}
--- src/sweet/pointer/stdafx.hpp
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-#define WIN32_LEAN_AND_MEAN
-#define _CRT_SECURE_NO_DEPRECATE
-#define _SCL_SECURE_NO_DEPRECATE
-#define NOMINMAX
