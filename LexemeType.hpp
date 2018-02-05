08a6b524676ed4dba73f03ee6a75f0ace5f62fcc
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-02-05 13:08:32 +1300

    Rename `SymbolLexemeType` to `LexemeType`

---
+++ LexemeType.hpp
@@ -0,0 +1,20 @@
+#ifndef SWEET_LALR_LEXEMETYPE_HPP_INCLUDED
+#define SWEET_LALR_LEXEMETYPE_HPP_INCLUDED
+
+namespace sweet
+{
+
+namespace lalr
+{
+
+enum LexemeType
+{
+    LEXEME_LITERAL,
+    LEXEME_REGULAR_EXPRESSION
+};
+
+}
+
+}
+
+#endif
