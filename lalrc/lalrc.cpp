f2cd4b1a8d464491740f7afaacb9a0be5983292a
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-03-13 20:54:18 +1300

    Add lalrc LALR parser compiler

---
+++ lalrc/lalrc.cpp
@@ -0,0 +1,236 @@
+//
+// lalrc.cpp
+// Copyright (c) Charles Baker. All rights reserved.
+//
+
+#include <sweet/lalr/Grammar.hpp>
+#include <sweet/lalr/ParserStateMachine.hpp>
+#include <sweet/lalr/ParserState.hpp>
+#include <sweet/lalr/ParserTransition.hpp>
+#include <sweet/lalr/ParserSymbol.hpp>
+#include <sweet/lalr/ParserAction.hpp>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+using std::string;
+using std::vector;
+using namespace sweet::lalr;
+
+int main( int argc, char** argv )
+{
+    string input;
+    string output;
+    bool help = false;
+    bool version = false;
+
+    int argi = 1;
+    while ( argi < argc )
+    {
+        if ( argv[argi][0] != '-' )
+        {
+            input = argv[argi];
+            argi += 1;
+        }
+        else if ( strcmp(argv[argi], "-o") == 0 || strcmp(argv[argi], "--output") == 0 )
+        {
+            output = argv[argi + 1];
+            argi += 2;
+        }
+        else if ( strcmp(argv[argi], "-h") == 0 || strcmp(argv[argi], "--help") == 0 )
+        {
+            help = true;
+            argi += 1;
+        }
+        else if ( strcmp(argv[argi], "-v") == 0 || strcmp(argv[argi], "--version") == 0 )
+        {
+            version = true;
+            argi += 1;
+        }
+    }
+
+    if ( version )
+    {
+        printf( "lalrc - LALR parser compiler\n" );
+        printf( "Copyright (c) Charles Baker. All rights reserved.\n" );
+    }
+
+    if ( help || input.empty() || output.empty() )
+    {
+        printf( "lalrc [options] -o|--output OUTPUT INPUT\n" );
+        printf( "-h|--help Display this help message\n" );
+        printf( "-v|--version Display version\n" );
+        printf( "-o|--output Output file\n" );
+    }
+
+    if ( !input.empty() && !output.empty() )
+    {
+        struct stat stat;
+        int result = ::stat( input.c_str(), &stat );
+        if ( result != 0 )
+        {
+            fprintf( stderr, "Stat failed on '%s' - result=%d\n", input.c_str(), result );
+            return EXIT_FAILURE;
+        }
+
+        FILE* file = fopen( input.c_str(), "rb" );
+        if ( !file )
+        {
+            fprintf( stderr, "Opening '%s' to read failed - errno=%d\n", input.c_str(), errno );
+            return EXIT_FAILURE;
+        }
+
+        int size = stat.st_size;
+        vector<char> grammar_source;
+        grammar_source.resize( size );
+        int read = fread( &grammar_source[0], sizeof(uint8_t), size, file );
+        fclose( file );
+        file = nullptr;
+        if ( read != size )
+        {
+            fprintf( stderr, "Reading grammar from '%s' failed - read=%d\n", input.c_str(), int(read) );
+            return EXIT_FAILURE;
+        }
+
+        Grammar grammar;
+        grammar.parse( &grammar_source[0], &grammar_source[0] + grammar_source.size() );
+
+        ParserStateMachine state_machine;
+        grammar.generate( &state_machine, nullptr, nullptr );
+
+        file = fopen( output.c_str(), "wb" );
+        if ( !file )
+        {
+            fprintf( stderr, "Opening '%s' to write parser state machine failed - errno=%d\n", output.c_str(), errno );
+            return EXIT_FAILURE;
+        }
+
+        fprintf( file, "\n" );
+        fprintf( file, "#include <sweet/lalr/ParserStateMachine.hpp>\n" );
+        fprintf( file, "#include <sweet/lalr/ParserState.hpp>\n" );
+        fprintf( file, "#include <sweet/lalr/ParserTransition.hpp>\n" );
+        fprintf( file, "#include <sweet/lalr/ParserSymbol.hpp>\n" );
+        fprintf( file, "#include <sweet/lalr/ParserAction.hpp>\n" );
+        fprintf( file, "\n" );
+        fprintf( file, "using namespace sweet::lalr;\n" );
+        fprintf( file, "\n" );
+        fprintf( file, "static const ParserAction actions [];\n" );
+        fprintf( file, "static const ParserSymbol symbols [];\n" );
+        fprintf( file, "static const ParserTransition transitions [];\n" );
+        fprintf( file, "static const ParserState states [];\n" );
+        fprintf( file, "\n" );
+
+        fprintf( file, "static const ParserAction actions [] = \n" );
+        fprintf( file, "{\n" );
+        const ParserAction* actions = state_machine.actions();
+        const ParserAction* actions_end = actions + state_machine.actions_size();
+        for ( const ParserAction* action = actions; action != actions_end; ++action )
+        {
+            fprintf( file, "    {%d, \"%s\"}%s\n", 
+                action->index, 
+                action->identifier,
+                action + 1 != actions_end ? "," : ""
+            );
+        }
+        fprintf( file, "};\n" );
+        fprintf( file, "\n" );
+
+        fprintf( file, "static const ParserSymbol symbols [] = \n" );
+        fprintf( file, "{\n" );
+        const ParserSymbol* symbols = state_machine.symbols();
+        const ParserSymbol* symbols_end = symbols + state_machine.symbols_size();
+        for ( const ParserSymbol* symbol = symbols; symbol != symbols_end; ++symbol )
+        {
+            fprintf( file, "    {%d, \"%s\", \"%s\", %d}%s\n", 
+                symbol->index, 
+                symbol->identifier, 
+                symbol->lexeme,
+                symbol->type,
+                symbol + 1 != symbols_end ? "," : ""
+            );
+        }
+        fprintf( file, "};\n" );
+        fprintf( file, "\n" );
+
+        fprintf( file, "static const ParserTransition transitions [] = \n" );
+        fprintf( file, "{\n" );
+        const ParserTransition* transitions = state_machine.transitions();
+        const ParserTransition* transitions_end = transitions + state_machine.transitions_size();
+        for ( const ParserTransition* transition = transitions; transition != transitions_end; ++transition )
+        {
+            if ( transition->reduced_symbol )
+            {
+                fprintf( file, "    {&symbols[%d], nullptr, &symbols[%d], %d, %d, %d, %d, %d}%s\n",
+                    transition->symbol ? transition->symbol->index : -1,
+                    transition->reduced_symbol->index,
+                    transition->reduced_length,
+                    transition->precedence,
+                    transition->action,
+                    transition->type,
+                    transition->index,
+                    transition + 1 != transitions_end ? "," : ""
+                );
+            }
+            else
+            {
+                fprintf( file, "    {&symbols[%d], &states[%d], nullptr, %d, %d, %d, %d, %d}%s\n",
+                    transition->symbol ? transition->symbol->index : -1,
+                    transition->state ? transition->state->index : -1,
+                    transition->reduced_length,
+                    transition->precedence,
+                    transition->action,
+                    transition->type,
+                    transition->index,
+                    transition + 1 != transitions_end ? "," : ""
+                );
+            }
+        }
+        fprintf( file, "};\n" );
+        fprintf( file, "\n" );
+
+        fprintf( file, "static const ParserState states [] = \n" );
+        fprintf( file, "{\n" );
+        const ParserState* states = state_machine.states();
+        const ParserState* states_end = states + state_machine.states_size();
+        for ( const ParserState* state = states; state != states_end; ++state )
+        {
+            fprintf( file, "    {%d, %d, &transitions[%d]}%s\n",
+                state->index,
+                state->length,
+                state->transitions->index,
+                state + 1 != states_end ? "," : "" 
+            );
+        }
+        fprintf( file, "};\n" );
+        fprintf( file, "\n" );
+
+        fprintf( file, "const ParserStateMachine %s_parser_state_machine = \n", grammar.identifier().c_str() );
+        fprintf( file, "{\n" );
+        fprintf( file, "    nullptr,\n" );
+        fprintf( file, "    nullptr,\n" );
+        fprintf( file, "    nullptr,\n" );
+        fprintf( file, "    nullptr,\n" );
+        fprintf( file, "    nullptr,\n" );
+        fprintf( file, "    nullptr,\n" );
+        fprintf( file, "    %d, // #actions\n", state_machine.actions_size() );
+        fprintf( file, "    %d, // #symbols\n", state_machine.symbols_size() );
+        fprintf( file, "    %d, // #transitions\n", state_machine.transitions_size() );
+        fprintf( file, "    %d, // #states\n", state_machine.states_size() );
+        fprintf( file, "    actions,\n" );
+        fprintf( file, "    symbols,\n" );
+        fprintf( file, "    transitions,\n" );
+        fprintf( file, "    states,\n" );
+        fprintf( file, "    &symbols[%d], // start symbol\n", state_machine.start_symbol()->index );
+        fprintf( file, "    &symbols[%d], // end symbol\n", state_machine.end_symbol()->index );
+        fprintf( file, "    &symbols[%d], // error symbol\n", state_machine.error_symbol()->index );
+        fprintf( file, "    &states[%d], // start state\n", state_machine.start_state()->index );
+        fprintf( file, "    nullptr, // lexer state machine\n" );
+        fprintf( file, "    nullptr // whitespace lexer state machine\n" );
+        fprintf( file, "};\n" );
+
+        fclose( file );
+        file = nullptr;
+    }
+}
