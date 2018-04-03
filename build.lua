e934f6523357bcb25a5f8ce1527c8ece84685713
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-04-03 19:49:23 +1200

    Add lalr/build.lua to build lalr without UnitTest++

---
+++ build.lua
@@ -0,0 +1,40 @@
+
+require 'build';
+require 'build.cc';
+require 'build.parser';
+require 'build.visual_studio';
+require 'build.xcode';
+require 'build.macos';
+require 'build.windows';
+
+platform = platform or build:operating_system();
+variant = variant or 'debug';
+version = version or ('%s %s %s'):format(os.date('%Y.%m.%d %H:%M:%S'), platform, variant );
+goal = goal or "";
+jobs = jobs or 4;
+
+build:initialize {
+    variants = { 'debug', 'release', 'shipping' };
+    bin = build:root( ('%s/bin'):format(variant) );
+    lib = build:root( ('%s/lib'):format(variant) );
+    obj = build:root( ('%s/obj'):format(variant) );
+    include_directories = {
+        build:root( '..' ),
+    };
+    library_directories = {
+        build:root( ('%s/lib'):format(variant) )
+    };
+    visual_studio = {
+        sln = build:root( 'lalr.sln' );
+    };
+    xcode = {
+        xcodeproj = build:root( 'lalr.xcodeproj' );
+    };
+};
+
+build:default_targets {
+    'lalrc',
+    'lalr_examples'
+};
+
+buildfile 'lalr.build';
