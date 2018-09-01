d877c66d2b192f8bfe491806646f0551b8a9d32e
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-09-01 15:20:00 +1200

    Update build scripts to work with latest Forge

--- lalr/forge/lalr/Lalrc.lua
+++ lalr/forge/lalr/Lalrc.lua
@@ -1,16 +1,14 @@
 
 local Lalrc = forge:TargetPrototype( 'Lalrc' );
 
-function Lalrc.create( forge, settings, identifier )
-    local target = forge:File( identifier, Lalrc, settings );
-    target:add_ordering_dependency( forge:Directory(forge:branch(target)) );
-    target:add_implicit_dependency( forge:file(settings.lalr.lalrc) );
-    target.settings = settings;
+function Lalrc.create( forge, identifier )
+    local target = forge:File( identifier, Lalrc );
+    target:add_implicit_dependency( forge:file(forge.settings.lalr.lalrc) );
     return target;
 end
 
 function Lalrc.build( forge, target )
-    local lalrc = target.settings.lalr.lalrc;
+    local lalrc = forge.settings.lalr.lalrc;
     forge:system( lalrc, ('lalrc -o "%s" "%s"'):format(target, target:dependency()) );
     target:add_implicit_dependency( forge:file(lalrc) );
 end
