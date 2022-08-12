47b3ed802034cdbb912ee7db08ad13e3c6958314
Charles Baker <charles@sweetsoftware.nz>
2022-08-12 16:07:26 +1200

    Call run() not system() in Lalrc.build()

--- src/lalr/forge/lalr/Lalrc.lua
+++ src/lalr/forge/lalr/Lalrc.lua
@@ -10,7 +10,7 @@ function Lalrc.build( toolset, target )
     local lalrc = target:dependency(1);
     local filename = target:dependency(2):filename();
     printf( leaf(filename) );
-    system( lalrc, ('lalrc -o "%s" "%s"'):format(target, filename) );
+    run( lalrc, ('lalrc -o "%s" "%s"'):format(target, filename) );
 end
 
 return Lalrc;
