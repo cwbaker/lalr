0e2089b1ce6ca5df45ae30cf35899697bdbbbe4a
Charles Baker <charles.baker@sweetsoftware.co.nz>
2023-05-27 16:26:56 +1200

    Remove GrammarProduction pointer from GrammarItem

--- src/lalr/GrammarItem.hpp
+++ src/lalr/GrammarItem.hpp
@@ -1,16 +1,8 @@
-#ifndef LALR_GRAMMARITEM_HPP_INCLUDED
-#define LALR_GRAMMARITEM_HPP_INCLUDED
-
-#include "GrammarSymbolLess.hpp"
-#include "GrammarSymbolSet.hpp"
-#include <string>
-#include <vector>
-#include <set>
+#pragma once
 
 namespace lalr
 {
 
-class GrammarSymbol;
 class GrammarProduction;
 
 /**
@@ -19,27 +11,19 @@ class GrammarProduction;
 */
 class GrammarItem
 {
-    unsigned short production_index_; ///< The index of the production of this item.
+    unsigned short production_; ///< The index of the production of this item.
     unsigned short position_; ///< The position of the dot in this item.
-    GrammarProduction* production_; ///< The production that this item is for.
     mutable int index_;
 
 public:
     GrammarItem();
     GrammarItem( GrammarProduction* production, int position );
     int index() const;
-    GrammarProduction* production() const;
+    int production() const;
     int position() const;
     bool dot_at_beginning() const;
-    bool dot_at_end() const;
-    bool next_node( const GrammarSymbol& symbol ) const;
-    const GrammarSymbol* next_symbol() const;
-    bool nullable_after_next() const;
-    std::string label() const;
     bool operator<( const GrammarItem& item ) const;
     void set_index( int index ) const;
 };
 
 }
-
-#endif
